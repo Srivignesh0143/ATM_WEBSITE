a53374049d3e1febec5e97eab3439c45982c7081
Srivignesh0143 <srivigneshsenthil6@gmail.com>
2026-06-16 18:19:50 +0530

    Updated API Url

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/atm", require("./routes/atmRoutes"));
+
 app.use((err, req, res, next) => {
   console.error(err.stack);
 
@@ -20,7 +22,7 @@ app.use((err, req, res, next) => {
     message: err.message,
   });
 });
-app.use("/api/atm", require("./routes/atmRoutes"));
+
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Server Running");
