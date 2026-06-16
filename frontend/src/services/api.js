775b6fe91e9929845321094b65670fd3c878141e
Srivignesh0143 <srivigneshsenthil6@gmail.com>
2026-06-16 18:33:16 +0530

    API URL error fixed

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API = "http://localhost:5001/api/atm";
+  import.meta.env.VITE_API_URL || "http://localhost:5001";
+
+const API = `${BASE_URL}/api/atm`;
 
 export const signup = (data) => {
   return axios.post(`${API}/signup`, data);
